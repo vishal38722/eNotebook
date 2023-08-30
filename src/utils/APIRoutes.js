60e153a9ccfe9b81110d0f42888a72a992cf7087
Vishal <vishal38722@gmail.com>
2023-08-30 23:36:14 +0530

    fixed 404 error while register

--- src/utils/APIRoutes.js
+++ src/utils/APIRoutes.js
@@ -1,5 +1,5 @@
-export const host = "http://localhost:5000";
-// export const host = "https://enotebook-backend-rej3.onrender.com";
+// export const host = "http://localhost:5000";
+export const host = "https://enotebook-backend-rej3.onrender.com";
 export const loginRoute = `${host}/api/auth/login`
 export const registerRoute = `${host}/api/auth/createuser`;
 
